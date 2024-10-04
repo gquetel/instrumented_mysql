1f9e5b49f6a88d33a043a34851fd03914ef0a678
gquetel <quetel.gregor@gmail.com>
2024-10-04 13:52:29 +0200

    Stopped tracking results and working init mysql script

--- init_mysql.sh
+++ init_mysql.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+DATA_PATH=/tmp/mysqld/datadir
 
-mkdir -p _mysql_files/data
-./mysql/result/bin/mysqld  --initialize-insecure --datadir=./_mysql_files/data/
-./mysql/result/bin/mysqld --datadir=./_mysql_files/data/ --socket=./_mysql_files/mysql.sock 
+# Check existence of /tmp/mysqld/datadir
+if [ -d "$DATA_PATH" ]; then
+  echo "Directory $DATA_PATH already exists... If you want to initialize the MySQL server, please remove the directory first."
+  exit 1 
+fi
+
+mkdir -p /tmp/mysqld/datadir
+
+./mysql/result/bin/mysqld   --datadir /tmp/mysqld/datadir --initialize-insecure
+./mysql/result/bin/mysqld --socket /tmp/mysqld/socket --datadir /tmp/mysqld/datadir
