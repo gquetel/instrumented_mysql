297b5c1bc7c0459c67c9aa14e3fb3f634f1edf77
gquetel <quetel.gregor@gmail.com>
2024-10-07 14:37:55 +0200

    updated bash script mysql

--- mysql/init_mysql.sh
+++ mysql/init_mysql.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-DATA_PATH=/tmp/mysqld/datadir
-SOCKET_PATH=/tmp/mysqld/socket
+PREFIX=/tmp/mysqld
+DATA_PATH=$PREFIX/datadir
+SOCKET_PATH=$PREFIX/socket
 # Check existence of /tmp/mysqld/datadir
 
 if [ -d "$DATA_PATH" ]; then
   echo "Directory $DATA_PATH already exists. If you want to reinitialize the MySQL server, remove the directory."
 else
-    mkdir -p /tmp/mysqld/datadir
-    ./result/bin/mysqld   --datadir /tmp/mysqld/datadir --initialize-insecure
+    mkdir -p $DATA_PATH
+    ./result/bin/mysqld   --datadir $DATA_PATH --initialize-insecure
 fi
 
 # This seems like a weird hack...
-if [ $(ps -aux | grep "/tmp/mysqld/socket" | wc -l) -eq 1 ]; then
-  ./result/bin/mysqld --socket $SOCKET_PATH --datadir /tmp/mysqld/datadir &
+if [ $(ps -aux | grep $SOCKET_PATH | wc -l) -eq 1 ]; then
+  ./result/bin/mysqld --socket $SOCKET_PATH --datadir $DATA_PATH &
   sleep 5 # Wait for the server to start  
+  # If the connection doesn't succeed, it means root password has already been executed: we already initialized it.
   ./result/bin/mysql --user=root   --socket $SOCKET_PATH < ./init_db.sql 
 else
   echo "MySQL server on that socket is already running."
 fi
 GAUR_LOG_PATH="$DATA_PATH/gaur.log"
 
-echo "GAUR log file is located at $GAUR_LOG_PATH"
