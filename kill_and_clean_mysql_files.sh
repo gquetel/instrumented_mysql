27b7f70246df2301a2e38c39ab8160ccc9fc355c
gquetel <quetel.gregor@gmail.com>
2024-10-18 13:45:10 +0200

    Fixed cleaning script

--- kill_and_clean_mysql_files.sh
+++ kill_and_clean_mysql_files.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 S_HOSTNAME=$(hostname -s)
-PREFIX=~/tmp/$S_HOSTNAME/mysqld
-
-DELETE_DIRS=0 # Do not delete by default
+DEL_PATH=~/tmp/$S_HOSTNAME/
+PREFIX=$DEL_PATH"mysqld"
 
 # Find PIDs of process using the PREFIX path and the --daemonize flag, i.e., the MySQL servers
 PID=$(ps -aux | grep $PREFIX | grep "daemonize" | awk '{print $2}')
@@ -10,26 +9,26 @@ PID=$(ps -aux | grep $PREFIX | grep "daemonize" | awk '{print $2}')
 # Check if results, if not, asks if the user wants to remove the directories
 if [ -z "$PID" ]; then
   echo "No MySQL server seems to be running."
-  exit 1  
 else
-  read -p "Do you want to remove the mysqld directories? [y/n] " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Yy]$ ]]; then
-    rm -rf $PREFIX"*"
-  fi
+  # Iterate over the PIDs
+  for p in $PID; do
+    # Get the command that started the process
+    COMM=$(cat /proc/${p}/comm)
+    # If comm is mysqld, kill the process
+    if [ "$COMM" == "mysqld" ]; then
+      kill -9 $p
+      echo "Killed process with PID $p"
+      # Also clean error files:
+      rm -f $PREFIX"_"$i"/datadir/$S_HOSTNAME.err"
+    else
+      echo "The process with PID $p is not mysqld ($COMM)."
+    fi
+  done
 fi
   
-# Iterate over the PIDs
-for p in $PID; do
-  # Get the command that started the process
-  COMM=$(cat /proc/${p}/comm)
-  # If comm is mysqld, kill the process
-  if [ "$COMM" == "mysqld" ]; then
-    kill -9 $p
-    echo "Killed process with PID $p"
-    # Also clean error files:
-    rm -f $PREFIX"_"$i"/datadir/$S_HOSTNAME.err"
-  else
-    echo "The process with PID $p is not mysqld ($COMM)."
-  fi
-done
+read -p "Do you want to remove the mysqld directories? [y/n] " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  rm -rf $DEL_PATH
+  echo "Directories removed."
+fi
